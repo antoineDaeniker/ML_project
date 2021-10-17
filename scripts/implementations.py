b3e91d4fce9adfd6235a7d869b873714a5af70db
xerox <antoine.daeniker@epfl.ch>
2021-10-17 16:25:10 +0200

    update pre-processing data

--- scripts/implementations.py
+++ scripts/implementations.py
@@ -32,10 +32,10 @@ def gradient_descent(y, tx, initial_w, max_iters, gamma):
         # store w and loss
         ws.append(w)
         losses.append(loss)
-        print("Gradient Descent({bi}/{ti}): loss={l}, w0={w0}, w1={w1}".format(
-              bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]))
+        print("Gradient Descent({bi}/{ti}): loss={l}".format(
+              bi=n_iter, ti=max_iters - 1, l=loss), end='\r')
 
-    return losses[len(losses) - 1], ws[len(ws) - 1]
+    return losses, ws[len(ws) - 1]
 
 
 def batch_iter(y, tx, batch_size, num_batches=1, shuffle=True):
@@ -136,32 +136,6 @@ def ridge_regression_demo(x, y, degree, ratio, seed):
     plot_train_test(rmse_tr, rmse_te, lambdas, degree)
     
     
-def ridge_regression_demo_2(x, y, degree, ratio, seed):
-    """ridge regression demo."""
-    # define parameter
-    lambdas = np.logspace(-5, 0, 15)
-
-    x_tr, y_tr, x_te, y_te = split_data(x, y, ratio, seed=seed)
-    print(x_tr.shape, y_tr.shape)
-    
-    #poly_data_tr = build_poly(x_tr, degree)
-    #poly_data_te = build_poly(x_te, degree)
-    
-    rmse_tr = []
-    rmse_te = []
-    for ind, lambda_ in enumerate(lambdas):
-        
-        w, l = ridge_regression(y_tr, x_tr, lambda_)
-        
-        rmse_tr.append(np.sqrt(2 * compute_loss(y_tr, x_tr, w)))
-        rmse_te.append(np.sqrt(2 * compute_loss(y_te, x_te, w)))
-        print("proportion={p}, degree={d}, lambda={l:.3f}, Training RMSE={tr:.3f}, Testing RMSE={te:.3f}".format(
-               p=ratio, d=degree, l=lambda_, tr=rmse_tr[ind], te=rmse_te[ind]))
-        
-    # Plot the obtained results
-    plot_train_test(rmse_tr, rmse_te, lambdas, degree)
-
-
 def split_data(x, y, ratio, seed=1):
     """
     split the dataset based on the split ratio. If ratio is 0.8 
@@ -177,25 +151,6 @@ def split_data(x, y, ratio, seed=1):
     index_te = indexes[index_split:]
     
     return x[index_tr], y[index_tr], x[index_te], y[index_te]
-
-
-def plot_train_test(train_errors, test_errors, lambdas, degree):
-    """
-    train_errors, test_errors and lambas should be list (of the same size) the respective train error and test error for a given lambda,
-    * lambda[0] = 1
-    * train_errors[0] = RMSE of a ridge regression on the train set
-    * test_errors[0] = RMSE of the parameter found by ridge regression applied on the test set
-    
-    degree is just used for the title of the plot.
-    """
-    plt.semilogx(lambdas, train_errors, color='b', marker='*', label="Train error")
-    plt.semilogx(lambdas, test_errors, color='r', marker='*', label="Test error")
-    plt.xlabel("lambda")
-    plt.ylabel("RMSE")
-    plt.title("Ridge regression for polynomial degree " + str(degree))
-    leg = plt.legend(loc=1, shadow=True)
-    leg.draw_frame(False)
-    plt.savefig("ridge_regression")
 
 
 def normalize(data):
