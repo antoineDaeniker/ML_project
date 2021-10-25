5bef36d02f36e1d002767ecd6fa4867261d3bcb7
xerox <antoine.daeniker@epfl.ch>
2021-10-25 08:24:50 +0200

    reggresion model demo

--- scripts/implementations.py
+++ scripts/implementations.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from proj1_helpers import *
 
 def compute_loss(y, tx, w):
     """Calculate the loss.
@@ -150,32 +151,30 @@ def split_data(x, y, ratio, seed=1):
     return x[index_tr], y[index_tr], x[index_te], y[index_te]
 
 
-def normalize(data):
-    mean = np.mean(data)
-    std = np.std(data)
-    return (data - mean) / std
-
 ############################################
-########### DATA PRE-PROCESSING ############
+########### NORMALIZE THE DATA #############
 ############################################
-def data_process(tX):
+def normalize_data(tX):
     temp = tX.copy()
-    tX_norm = np.zeros(tX.shape)
     temp[temp == -999] = 0
     mean_features = np.mean(temp, axis=0)
     std_features = np.std(temp, axis=0)
-    for i, f in enumerate(tX.T):
+    std_0_feat_ind = np.squeeze(np.argwhere(std_features <= 0.001))
+    mean_features = np.delete(mean_features, std_0_feat_ind)
+    std_features = np.delete(std_features, std_0_feat_ind)
+    data_reduce = np.delete(tX, std_0_feat_ind, axis=1)
+    data_norm = np.zeros(data_reduce.shape)
+    for i, f in enumerate(data_reduce.T):
         f[f == -999] = mean_features[i]
-        #tX_norm[:, i] = f
-        tX_norm[:, i] = (f - mean_features[i]) / std_features[i]
+        data_norm[:, i] = (f - mean_features[i]) / std_features[i]
         
-    return np.array(tX_norm)
+    return np.array(data_norm), np.array(std_0_feat_ind)
 
 
 ############################################
 ###### FEATURES CORRELATION SELECTION ######
 ############################################
-def feature_selection(tX, threshold, show_plot=False):
+def feature_correlation(tX, threshold, show_plot=False):
     corr_mat = np.corrcoef(tX, rowvar=False)
     
     if show_plot:
@@ -183,14 +182,14 @@ def feature_selection(tX, threshold, show_plot=False):
         plt.colorbar()
         plt.show()
         
-    corr_indices = np.argwhere(np.abs(np.triu(corr_mat - np.eye(30))) > threshold)
+    corr_indices = np.argwhere(np.abs(np.triu(corr_mat - np.eye(tX.shape[1]))) > threshold)
     
     unique_ind1 = np.unique(corr_indices[:, 0])
     unique_ind2 = np.unique(corr_indices[:, 1])
     len1 = len(unique_ind1)
     len2 = len(unique_ind2)
     corr_ind_reduce_short = (unique_ind1, unique_ind2)[len(unique_ind1) > len(unique_ind2)]
-    corr_ind_reduce_big = (unique_ind1, unique_ind2)[len(unique_ind1) < len(unique_ind2)]
+    corr_ind_reduce_big = (unique_ind1, unique_ind2)[len(unique_ind1) <= len(unique_ind2)]
     corr_ind_to_keep = []
     for ind in corr_ind_reduce_short:
         is_in = np.isin(ind, corr_ind_reduce_big)
@@ -200,13 +199,15 @@ def feature_selection(tX, threshold, show_plot=False):
     all_ind = np.unique(corr_indices.flatten())
     corr_ind_to_throw = np.setdiff1d(all_ind, corr_ind_to_keep)
     
-    return np.array(corr_ind_to_keep), corr_ind_to_throw
+    data_reduce = np.delete(tX, corr_ind_to_throw, axis=1)
+    
+    return data_reduce, corr_ind_to_throw, np.array(corr_ind_to_keep)
 
 
 ###############################################################
 ###### DIVIDE DATA W.R.T FEATURE WITH LOW VARIATION DATA ######
 ###############################################################
-def subdivide_data(tX):
+def subdivide_data(tX, y):
     #Find the nb of different value in per feature
     nb_diff_values = []
     for feat in tX.T:
@@ -215,30 +216,75 @@ def subdivide_data(tX):
     #Create the different dataset w.r.t those different value (and remove the feature concern)
     feat_ind = np.argmin(nb_diff_values)
     feat_values = np.unique(tX[:, feat_ind])
-    data = [0] * len(feat_values)
+    data_list = []
+    y_list = []
     for i, val in enumerate(feat_values):
-        data[i] = np.delete(tX[tX[:, feat_ind] == val], feat_ind, axis=1)
+        data_list.append(np.delete(tX[tX[:, feat_ind] == val], feat_ind, axis=1))
+        y_list.append(y[tX[:, feat_ind] == val])
     
+    return data_list, y_list, feat_ind
+
+
+################################################################
+### DELETE FEATURE WITH MORE THAN THRESHOLD OF CORRUPT DATA ####
+################################################################
+def delete_irr_features(data, threshold):
     #Remove irrelevant feature, for each data set
     count_999 = []
-    for sub_data in data:
-        sub_count_999 = []
-        for feat in sub_data.T:
-            sub_count_999.append(len(feat[feat == -999]) / len(feat))
-        count_999.append(sub_count_999)
+    for feat in data.T:
+        count_999.append(len(feat[feat == -999]) / len(feat))
+    
+    count_999_ind = np.squeeze(np.argwhere(np.array(count_999) > threshold))
+    data_reduce = np.delete(data, np.array(count_999_ind), axis=1)
+    
+    return data_reduce, count_999_ind
+
+################################################################
+#################### PRE-PROCESS THE DATA ######################
+################################################################
+def data_train_preprocessing(tX, y, threshold_irr, threshold_corr, show_plot=False):
+    
+    data_list, y_list, feat_ind = subdivide_data(tX, y)
+    
+    data_reduce_list = []
+    rmv_feat_idx_list = []
+    for i, data in enumerate(data_list):
+        data_reduce, irr_idx = delete_irr_features(data, threshold_irr)
+        data_reduce, corr_idx, _ = feature_correlation(data_reduce, threshold_corr)
+        data_reduce, std_0_feat_ind = normalize_data(data_reduce)
+
+        print("train data {} shape : {}".format(i, data_reduce.shape))
+        data_reduce_list.append(data_reduce)
+
+        rmv_feat_idx = np.unique(np.concatenate((irr_idx, corr_idx)))
+        rmv_feat_idx = np.insert(rmv_feat_idx, -1, std_0_feat_ind)
+        rmv_feat_idx = np.insert(rmv_feat_idx, -1, feat_ind)
+        rmv_feat_idx_list.append(np.unique(rmv_feat_idx))
         
-    count_999_ind = []
-    for sub_count in count_999:
-        indices = np.argwhere(np.array(sub_count) > 0.5)
-        #if len(indices) > 0:
-        count_999_ind.append(np.argwhere(np.array(sub_count) > 0.5))
-        
-    data_reduce = [0] * len(feat_values)
-    for i, indices in enumerate(count_999_ind):
-        data_reduce[i] = np.delete(data[i], np.array(indices), axis=1)
-        
-    return data_reduce
+    return data_reduce_list, y_list, rmv_feat_idx_list
 
 
+def data_test_preprocessing(tX_test, y_test, rmv_feat_idx_list, threshold_irr, threshold_corr, show_plot=False):
+    
+    data_list_test, y_list_test, feat_ind = subdivide_data(tX_test, y_test)
+    
+    new_data_test_list = []
+    for rmv_feat_idx, data_test in zip(rmv_feat_idx_list, data_list_test):
+        data_test_reduce = np.delete(data_test, rmv_feat_idx, axis=1)
+        norm_data_test_reduce, _ = normalize_data(data_test_reduce)
+        
+        print(norm_data_test_reduce.shape)
+        new_data_test_list.append(norm_data_test_reduce)
+        
+    return new_data_test_list, y_list_test
+    
+
+def accuracy(w, data, y, y_test_len):
+    pred = predict_labels(w, data)
+    accuracy = len(pred[pred == y]) * 100 / y_test_len
+        
+    return accuracy
+    
+    
 
 
